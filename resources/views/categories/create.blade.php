e0d19e4820bb85aad65dc890cef94e521d004eb7
hectic-vyash <vyashbhawan@hotmail.com>
2019-06-15 23:13:23 +0200

    25: categories can be stored

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card card-default">
+        <div class="card-header">Create category</div>
+        <div class="card-body">
+            @if($errors->any())
+                <ul class="list-group">
+                    @foreach($errors->all() as $error)
+                        <li class="list-group-item text-danger">
+                            {{$error}}
+                        </li>
+                    @endforeach
+                </ul>
+            @endif
+            <form action="{{route('categories.store')}}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" id="name" class="form-control" name="name">
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-outline-success">Add category</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection('content')
