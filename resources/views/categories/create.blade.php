dc925eb6372d7f2350a21fd85589d8f0f6ce7db6
hectic-vyash <vyashbhawan@hotmail.com>
2019-06-17 10:45:52 +0200

    27: added category update function

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
     <div class="card card-default">
-        <div class="card-header">Create category</div>
+        <div class="card-header">
+            {{isset($category) ? 'Edit category' : 'Create category'}}
+        </div>
         <div class="card-body">
             @if($errors->any())
                 <ul class="list-group">
@@ -13,14 +15,20 @@
                     @endforeach
                 </ul>
             @endif
-            <form action="{{route('categories.store')}}" method="POST">
+            <form action="{{isset($category) ? route('categories.update', $category->id): route('categories.store')}}" method="POST">
                 @csrf
+                @if(isset($category))
+                    @method('PUT')
+                @endif
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="text" id="name" class="form-control" name="name">
+                    <input type="text" id="name" class="form-control" name="name"
+                           value="{{isset($category) ? $category->name : ''}}">
                 </div>
                 <div class="form-group">
-                    <button class="btn btn-outline-success">Add category</button>
+                    <button class="btn btn-outline-success">
+                        {{isset($category) ? 'Save' : 'Add category'}}
+                    </button>
                 </div>
             </form>
         </div>
